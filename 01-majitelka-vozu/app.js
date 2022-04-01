eac80f63b82d19c2a24cb912b5a3e7fdae4c1dee
Marketa Majerova <marketamajerova.6@gmail.com>
2022-04-01 23:44:59 +0200

    first version

--- 01-majitelka-vozu/app.js
+++ 01-majitelka-vozu/app.js
@@ -1,10 +1,25 @@
 // Vytvoř objekt auto, který bude mít vlastnosti: znacka, barva, rokVyroby,..
-
-
+let auto = {
+    znacka: 'volvo',
+    barva: 'bila',
+    rokVyroby: 2020,
+    spz: '1B2 3456'
+};
+console.log(auto);
 // Přidej novou vlastnost (majitel a prirad ji svoje jmeno jako hodnotu)
 
-
+auto.majitel = {};
+auto.majitel.jmeno = 'Marketa';
+auto.majitel.prijimani = 'Majerova';
 // Přepiš značku na Škoda
 
+auto.znacka = 'Skoda';
 
 // Vytvor tri promenne majitelka, znacka a spz a vloz do techto elementu text z objektu auto.
+console.log(auto);
+
+let majitelka = auto.majitel.jmeno + ' ' + auto.majitel.prijimani;
+let znacka = auto.znacka;
+let spz = auto.spz;
+
+console.log(majitelka);
